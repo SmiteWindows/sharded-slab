3f91b207334f99f3723cda9c8aca4700621decea
Smite <1627283564@qq.com>
2026-02-02 19:16:09 +0800

    fix and fmt

--- src/shard.rs
+++ src/shard.rs
@@ -206,21 +206,20 @@ where
         crate::sync::atomic::fence(crate::sync::atomic::Ordering::Acquire);
 
         let tid = Tid::<C>::current();
-
         if tid.is_poisoned() {
-            self.clear_remote(idx);
+            return;
+        }
+
+        let tid_usize = tid.as_usize();
+        test_println!(
+            "-> clear_after_release; self.tid={:?}; current.tid={:?};",
+            tid_usize,
+            self.tid
+        );
+        if tid_usize == self.tid {
+            self.clear_local(idx);
         } else {
-            let tid_usize = tid.as_usize();
-            test_println!(
-                "-> clear_after_release; self.tid={:?}; current.tid={:?};",
-                tid_usize,
-                self.tid
-            );
-            if tid_usize == self.tid {
-                self.clear_local(idx);
-            } else {
-                self.clear_remote(idx);
-            }
+            self.clear_remote(idx);
         }
     }
 
